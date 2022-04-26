eff86dce4d61dd57af73176c5fb336e5f345bd2f
Your Name <you@example.com>
2022-04-26 14:25:52 +0700

    Prepare for start

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,20 +15,22 @@ module.exports = {
    */
   plugins: [
     {
-      /**
-       * First up is the WordPress source plugin that connects Gatsby
-       * to your WordPress site.
-       *
-       * visit the plugin docs to learn more
-       * https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-source-wordpress/README.md
-       *
-       */
       resolve: `gatsby-source-wordpress`,
       options: {
-        // the only required plugin option for WordPress is the GraphQL url.
-        url:
-          process.env.WPGRAPHQL_URL ||
-          `https://wpgatsbydemo.wpengine.com/graphql`,
+        url: `https://musosoft.com/graphql`,
+        develop: {
+          hardCacheMediaFiles: true,
+        },
+        type: {
+          MediaItem: {
+            localFile: {
+              requestConcurrency: 30,
+            },
+          },
+        },
+        schema: {
+          requestConcurrency: 5,
+        },
       },
     },
 
